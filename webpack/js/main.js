adfe6d868c0aaffbbb3d119fbb5449ceae0eb52f
Seio <seio@Daniels-MacBook-Pro.local>
2019-12-03 21:14:11 +0100

    remove cookie notice

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,7 +1,5 @@
 import initNavigation from "./navigation";
-import initCookieConsent from "./cookie_notice";
 import initNetlify from "./netlify";
 
 initNavigation();
-initCookieConsent();
 initNetlify();
