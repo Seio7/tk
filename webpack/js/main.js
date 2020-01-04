b3682659be2639fe93bb6062c77a8ecbc8f376f6
Seio <yuusn@hotmail.com>
2020-01-04 04:19:50 +0100

    add intersection overserver poll interval

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -3,6 +3,7 @@ import initNetlify from "./netlify";
 import Highway from './highway';
 import app from './app';
 
+IntersectionObserver.prototype.POLL_INTERVAL = 100;
 initNavigation();
 initNetlify();
 Highway();
