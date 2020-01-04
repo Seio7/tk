e72b02564ed29c77db8eddd7f7a5e3e3bb39947c
Seio <seio@Daniels-MacBook-Pro.local>
2020-01-04 18:34:59 +0100

    ie fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "production",
   watch: false,
-  entry: [path.join(__dirname, "webpack", "js", "main")],
+  entry: path.join(__dirname, "webpack", "js", "main.js"),
   output: {
     filename: "[name]-bundle.js",
     path: path.resolve(__dirname, "assets/js"),
@@ -15,9 +15,9 @@ module.exports = {
         // exclude: [
         //   path.resolve(__dirname, "node_modules"),
         // ],
-        include: [
-          path.resolve(__dirname, 'node_modules', '@dogstudio', 'highway', 'src')
-        ],
+        // include: [
+        //   path.resolve(__dirname, 'node_modules', '@dogstudio', 'highway', 'src')
+        // ],
         use: {
           loader: 'babel-loader',
           options: {
