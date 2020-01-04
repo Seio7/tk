f39aaa89e28178250bc03a44c3bde71183e61238
Seio <seio@Daniels-MacBook-Pro.local>
2020-01-04 06:08:36 +0100

    attempt to fix not transpiling nav file

--- webpack/js/navigation.js
+++ webpack/js/navigation.js
@@ -154,11 +154,9 @@ export default function initNavigation() {
 
   function closeMenuOnNavigation() {
     const links = document.querySelectorAll(".menu-item a")
-    links.forEach(link => {
-      window.innerWidth <= 1000 
-        ? link.addEventListener("click", closeMobileMenu)
-        : link.removeEventListener("click", closeMobileMenu);
-    })
+    for (link of links) {
+      window.innerWidth <= 1000 ? link.addEventListener("click", closeMobileMenu) : link.removeEventListener("click", closeMobileMenu);
+    }
   }
   closeMenuOnNavigation();
 
